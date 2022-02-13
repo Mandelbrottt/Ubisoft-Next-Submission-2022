644d2a7ade3fa49cbd67bcedfff4535866e13c08
Myles Cragg <cragg.myles@gmail.com>
2022-02-12 22:14:00 -0500

    Added namespace define for better flexibility down the line

--- NextCore/NextCore/Reflection/Reflection.h
+++ NextCore/NextCore/Reflection/Reflection.h
@@ -19,18 +19,20 @@
 	#define _REFLECT_TYPE_ALIAS This
 	#define _REFLECT_BASE_ALIAS Base
 
+	#define _REFLECT_NAMESPACE ::NextCore::Reflection::
+
 	#define _REFLECT_DECLARE_COMMON(_class_) \
 		private: \
-			friend class ::NextCore::Reflection::Type; \
+			friend class _REFLECT_NAMESPACE Type; \
 			typedef _class_ _REFLECT_TYPE_ALIAS; \
 		public: \
-			static ::NextCore::Reflection::Type& GetType() \
+			static _REFLECT_NAMESPACE Type& GetType() \
 			{ \
-				return ::NextCore::Reflection::Type::Get<_REFLECT_TYPE_ALIAS>();\
+				return _REFLECT_NAMESPACE Type::Get<_REFLECT_TYPE_ALIAS>();\
 			} \
-			static ::NextCore::Reflection::Type* TryGetType() \
+			static _REFLECT_NAMESPACE Type* TryGetType() \
 			{ \
-				return ::NextCore::Reflection::Type::TryGet<_REFLECT_TYPE_ALIAS>();\
+				return _REFLECT_NAMESPACE Type::TryGet<_REFLECT_TYPE_ALIAS>();\
 			}
 
 	#define _REFLECT_DECLARE_1(_class_) \
@@ -43,27 +45,27 @@
 		private: \
 			typedef _base_ _REFLECT_BASE_ALIAS; \
 		public: \
-			static ::NextCore::Reflection::Type& GetBaseType() \
+			static _REFLECT_NAMESPACE Type& GetBaseType() \
 			{ \
-				return ::NextCore::Reflection::Type::Get<_REFLECT_BASE_ALIAS>();\
+				return _REFLECT_NAMESPACE Type::Get<_REFLECT_BASE_ALIAS>();\
 			} \
-			static ::NextCore::Reflection::Type* TryGetBaseType() \
+			static _REFLECT_NAMESPACE Type* TryGetBaseType() \
 			{ \
-				return ::NextCore::Reflection::Type::TryGet<_REFLECT_BASE_ALIAS>();\
+				return _REFLECT_NAMESPACE Type::TryGet<_REFLECT_BASE_ALIAS>();\
 			}
 
 	#define REFLECT_DECLARE(...) _MACRO_OVERLOAD(_REFLECT_DECLARE, __VA_ARGS__)
 
 	#define REFLECT_MEMBERS(_list_) \
 		private:\
-			friend class ::NextCore::Reflection::Type; \
+			friend class _REFLECT_NAMESPACE Type; \
 			static \
 			void \
-			Reflect(::NextCore::Reflection::Type& r) \
+			Reflect(_REFLECT_NAMESPACE Type& r) \
 			{ \
 				r _list_; \
 				\
-				if constexpr (::NextCore::Reflection::is_complete_type_v<Base>)\
+				if constexpr (_REFLECT_NAMESPACE is_complete_type_v<Base>)\
 				{\
 					r.operator()<Base, This>();\
 				}\
@@ -71,15 +73,15 @@
 
 	#pragma region Reflect Field Overloads
 		#define _REFLECT_FIELD_PROTOTYPE(_field_, ...) \
-			(::NextCore::Reflection::Field {\
+			(_REFLECT_NAMESPACE Field {\
 				/*.debugName        =*/ #_field_,\
-				/*.fieldName        =*/ ::NextCore::Reflection::Pick<::NextCore::Reflection::Name>(__VA_ARGS__, ::NextCore::Reflection::Name(#_field_)).c_str,\
-				/*.fieldDescription =*/ ::NextCore::Reflection::Pick<::NextCore::Reflection::Description>(__VA_ARGS__, ::NextCore::Reflection::Description("")).c_str,\
+				/*.fieldName        =*/ _REFLECT_NAMESPACE Pick<_REFLECT_NAMESPACE Name>(__VA_ARGS__, _REFLECT_NAMESPACE Name(#_field_)).c_str,\
+				/*.fieldDescription =*/ _REFLECT_NAMESPACE Pick<_REFLECT_NAMESPACE Description>(__VA_ARGS__, _REFLECT_NAMESPACE Description("")).c_str,\
 				/*.offset           =*/ (uint32_t) _REFLECT_OFFSET_OF(_REFLECT_TYPE_ALIAS, _field_), \
 				/*.size             =*/ (uint32_t) sizeof(_field_), \
 				/*.fieldType        =*/ typeid(decltype(_field_)), \
 				/*.containingType   =*/ typeid(_REFLECT_TYPE_ALIAS),\
-				/*.flags            =*/ ::NextCore::Reflection::Pick<::NextCore::Reflection::FieldFlags>(__VA_ARGS__, ::NextCore::Reflection::FfNone)\
+				/*.flags            =*/ _REFLECT_NAMESPACE Pick<_REFLECT_NAMESPACE FieldFlags>(__VA_ARGS__, _REFLECT_NAMESPACE FfNone)\
 			})
 
 		#define _REFLECT_FIELD_1(_field_) _REFLECT_FIELD_2(_field_, #_field_)
@@ -111,7 +113,7 @@
 	#define REFLECT_REGISTER(_type_) \
 		static int _REFLECT_REGISTER_##_type_ = []() \
 		{ \
-			::NextCore::Reflection::Type::Register<_type_>();\
-			return ::NextCore::Reflection::GetStaticId<_type_>(); \
+			_REFLECT_NAMESPACE Type::Register<_type_>();\
+			return _REFLECT_NAMESPACE GetStaticId<_type_>(); \
 		}();
 #pragma endregion
