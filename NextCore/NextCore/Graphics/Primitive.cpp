50e2e689447208b5c1fbec0e6b168477d8bb9a71
Myles Cragg <cragg.myles@gmail.com>
2022-02-18 21:25:33 -0500

    Moved rendering stuffs from Sprite to Primitive

---
+++ NextCore/NextCore/Graphics/Primitive.cpp
@@ -0,0 +1,229 @@
+#include "pch.h"
+
+#include "Primitive.h"
+
+#include "Application/Time.h"
+
+#include "Math/Transformations.h"
+
+namespace NextCore::Graphics
+{
+	void
+	Primitive::OnUpdate(float a_deltaTime)
+	{
+		if (!IsValid())
+		{
+			return;
+		}
+
+		m_sprite->Update(a_deltaTime);
+	}
+
+	bool
+	Primitive::LoadFromTexture(std::string_view a_fileName, unsigned a_nColumns, unsigned a_nRows)
+	{
+		m_sprite = new CSimpleSprite(a_fileName.data(), a_nColumns, a_nRows);
+
+		if (!m_sprite)
+		{
+			return false;
+		}
+
+		SetFrame(0);
+
+		return true;
+	}
+
+	void
+	Primitive::OnRenderPrepare(
+		Math::Matrix4 const& a_model,
+		Math::Matrix4 const& a_view,
+		Math::Matrix4 const& a_projection
+	)
+	{
+		if (!IsValid())
+		{
+			return;
+		}
+
+		using namespace Math;
+
+		//// Model Matrix
+		//auto* transform = Transform();
+		//auto& scale    = transform->Scale();
+		//auto& rotation = transform->Rotation();
+		//auto& position = transform->Position();
+
+		//auto model = Matrix4::Identity();
+		//model *= Scale(scale);
+		//model *= RotateZ(rotation.z);
+		//model *= RotateY(rotation.y);
+		//model *= RotateX(rotation.x);
+		//model *= Translate(position);
+
+		//// View Matrix
+
+		//// Perspective Matrix
+		//float fov = 90;
+		//float aspect = 16.f / 9.f;
+		//
+		//auto perspective = Perspective(fov, aspect, 0.1f, 1000.f);
+
+		float runningDepthTotal = 0.0f;
+
+		for (int i = 0; i < 4; i++)
+		{
+			Vector4 vertex = { m_vertices[i], 1.0f };
+
+			auto transformedVertex = a_model * vertex;
+			auto projectedVertex   = a_projection * transformedVertex;
+
+			projectedVertex.x /= projectedVertex.w;
+			projectedVertex.y /= projectedVertex.w;
+			projectedVertex.z /= projectedVertex.w;
+
+			//projectedVertex.x *= aspect;
+
+			m_sprite->SetVertex(2 * i, projectedVertex.x);
+			m_sprite->SetVertex(2 * i + 1, projectedVertex.y);
+
+			runningDepthTotal += projectedVertex.z;
+		}
+
+		m_depth = runningDepthTotal / 4;
+	}
+
+	void
+	Primitive::OnRender()
+	{
+		if (!IsValid())
+		{
+			return;
+		}
+
+		m_sprite->Draw();
+	}
+
+	unsigned
+	Primitive::GetFrame() const
+	{
+		if (!IsValid())
+		{
+			return {};
+		}
+
+		return m_sprite->GetFrame();
+	}
+
+	Math::Vector3
+	Primitive::GetVertex(unsigned a_index) const
+	{
+		if (!IsValid())
+		{
+			return {};
+		}
+
+		Math::Vector3 result = m_vertices[a_index];
+
+		return result;
+	}
+
+	Math::Vector2
+	Primitive::GetUv(unsigned a_index) const
+	{
+		if (!IsValid())
+		{
+			return {};
+		}
+
+		// Multiply input index by 2 because underlying api uses individual floats instead of structs
+		unsigned x_index = a_index * 2;
+		unsigned y_index = x_index + 1;
+
+		Math::Vector2 result {
+			m_sprite->GetUv(x_index),
+			m_sprite->GetUv(y_index)
+		};
+
+		return result;
+	}
+
+	void
+	Primitive::SetFrame(unsigned a_frame)
+	{
+		if (!IsValid())
+		{
+			return;
+		}
+
+		m_sprite->SetFrame(a_frame);
+	}
+
+	void
+	Primitive::SetAnimation(unsigned a_id)
+	{
+		if (!IsValid())
+		{
+			return;
+		}
+
+		m_sprite->SetAnimation(a_id);
+	}
+
+	void
+	Primitive::SetColor(Math::Vector3 a_color)
+	{
+		if (!IsValid())
+		{
+			return;
+		}
+
+		m_sprite->SetColor(a_color.r, a_color.g, a_color.b);
+	}
+
+	void
+	Primitive::CreateAnimation(unsigned a_id, float a_speed, const std::vector<int>& a_frames)
+	{
+		if (!IsValid())
+		{
+			return;
+		}
+
+		m_sprite->CreateAnimation(a_id, a_speed, a_frames);
+	}
+
+	void
+	Primitive::SetVertex(unsigned a_index, Math::Vector3 a_value)
+	{
+		if (!IsValid())
+		{
+			return;
+		}
+
+		// Don't set the sprite's vertices; We maintain model space
+		// vertices that are used to calculate the sprite's vertices per frame
+		m_vertices[a_index] = a_value;
+	}
+
+	void
+	Primitive::SetUv(unsigned a_index, Math::Vector2 a_value)
+	{
+		if (!IsValid())
+		{
+			return;
+		}
+
+		// Multiply input index by 2 because underlying api uses individual floats instead of structs
+		unsigned x_index = a_index * 2;
+		unsigned y_index = x_index + 1;
+
+		m_sprite->SetUv(x_index, a_value.x);
+		m_sprite->SetUv(y_index, a_value.y);
+	}
+
+	bool
+	Primitive::IsValid() const
+	{
+		return m_sprite != nullptr;
+	}
+}
---
+++ NextCore/NextCore/Graphics/Primitive.h
@@ -0,0 +1,138 @@
+#pragma once
+
+// Keep to avoid errors when included
+#include <string_view>
+
+#include "Math/Vector.h"
+#include "Math/Matrix4.h"
+
+class CSimpleSprite;
+
+namespace NextCore::Graphics
+{
+	enum class PrimitiveType
+	{
+		// TODO: Add support for triangles
+		Triangle,
+		Quad,
+	};
+	
+	class Primitive final
+	{
+		friend class Mesh;
+		
+		Primitive() = default;
+		~Primitive() = default;
+	public:
+		void
+		OnUpdate(float a_deltaTime);
+		
+		/**
+		 * \brief Load an image from the given file. The image can be a sprite sheet.
+		 * \param a_fileName The name of the image file.
+		 * \param a_nColumns If the image is a sprite sheet, the number of columns in the sheet.
+		 * \param a_nRows    If the image is a sprite sheet, the number of rows in the sheet.
+		 * \return True if the image was loaded successfully, false otherwise.
+		 * \remark Currently only supports .bmp files.
+		 */
+		bool
+		LoadFromTexture(std::string_view a_fileName, unsigned int a_nColumns = 1, unsigned int a_nRows = 1);
+		
+		/**
+		 * \brief Prepare the primitive for rendering
+		 */
+		void
+		OnRenderPrepare(Math::Matrix4 const& a_model, Math::Matrix4 const& a_view, Math::Matrix4 const& a_projection);
+
+		/**
+		 * \brief Render the sprite to the screen
+		 */
+		void
+		OnRender();
+		
+		/**
+		 * \return The average depth of this sprite
+		 * \remark only valid on any given frame after the call to \link OnRenderPrepare \endlink
+		 */
+		float
+		GetDepth() const
+		{
+			return m_depth;
+		}
+		
+		/**
+		 * \return The frame number that the animation is currently on 
+		 */
+		unsigned int
+		GetFrame() const;
+
+		/**
+		 * \param a_index The index of quad or triangle's vertex in the range [0, 3] or [0, 2] respectively
+		 * \return The value of the vertex
+		 */
+		Math::Vector3
+		GetVertex(unsigned int a_index) const;
+		
+		/**
+		 * \param a_index The index of quad's Uv in the range [0, 3]
+		 * \return The value of the uv
+		 */
+		Math::Vector2
+		GetUv(unsigned int a_index) const;
+		
+		/**
+		 * \param a_frame The frame of animation to move to. 
+		 */
+		void
+		SetFrame(unsigned int a_frame);
+
+		/**
+		 * \brief Switch to the animation with id a_id
+		 * \param a_id The id of the animation to switch to. 
+		 */
+		void
+		SetAnimation(unsigned int a_id);
+
+		/**
+		 * \brief Set the color that the texture will blend with.
+		 * \param a_color A color.
+		 * \remark Great for primitive lighting
+		 */
+		void
+		SetColor(Math::Vector3 a_color);
+
+		/**
+		 * \brief Create an animation reel that can be changed to later.
+		 * \param a_id A unique animation Id. Used to change to the animation with \link SetAnimation \endlink
+		 * \param a_speed The time in seconds between frames
+		 * \param a_frames A list of the zero-base indexed frames, where 0 is top-left
+		 *        and moves left to right, top to bottom
+		 */
+		void
+		CreateAnimation(unsigned int a_id, float a_speed, const std::vector<int>& a_frames);
+
+		/**
+		 * \param a_index The index of the quad's vertex in the range [0, 3]
+		 * \param a_value The value of the vertex
+		 */
+		void
+		SetVertex(unsigned int a_index, Math::Vector3 a_value);
+		
+		/**
+		 * \param a_index The index of the quad's uv in the range [0, 3]
+		 * \param a_value The value of the uv
+		 */
+		void
+		SetUv(unsigned int a_index, Math::Vector2 a_value);
+
+		bool
+		IsValid() const;
+
+	private:
+		::CSimpleSprite* m_sprite;
+
+		Math::Vector3 m_vertices[4];
+
+		float m_depth;
+	};
+}
