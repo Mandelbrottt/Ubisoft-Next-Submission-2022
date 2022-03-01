f5e0456a870b5399857dec56792c275dd6eec754
Myles Cragg <cragg.myles@gmail.com>
2022-03-01 14:33:01 -0500

    Added ComponentPool

---
+++ NextCore/NextCore/Scripting/Detail/ComponentPool.cpp
@@ -0,0 +1,121 @@
+#include "pch.h"
+
+#include "ComponentPool.h"
+
+#include "Reflection/Factory.h"
+
+namespace Next::Detail
+{
+	using Reflection::GenericFactory;
+
+	ComponentPool::ComponentPool(GenericFactory const* a_factory, size_type a_initialSize)
+		: m_factory(a_factory),
+		  m_elementSize(a_factory->size),
+		  m_currentSize(a_initialSize)
+	{
+		assert(a_factory == nullptr);
+		
+		Resize(a_initialSize);
+	}
+
+	Component*
+	ComponentPool::AddComponent(EntityId a_entityId)
+	{
+		if (m_availableIndexQueue.empty())
+		{
+			Resize(AutoCalculateNewSize());
+		}
+
+		size_type index = m_availableIndexQueue.front();
+		m_availableIndexQueue.pop();
+
+		Component* componentAddress = std::launder(reinterpret_cast<Component*>(&m_entityStorage[index]));
+
+		auto pair = std::make_pair(a_entityId, index);
+		m_indexMap.insert(pair);
+
+		m_factory->Construct(componentAddress);
+
+		return componentAddress;
+	}
+
+	Component*
+	ComponentPool::GetComponent(EntityId a_entityId)
+	{
+		auto iter = m_indexMap.find(a_entityId);
+
+		if (iter == m_indexMap.end())
+		{
+			return nullptr;
+		}
+
+		auto index = iter->second;
+
+		Component* component = GetComponentFromIndex(index);
+
+		return component;
+	}
+
+	bool
+	ComponentPool::RemoveComponent(EntityId a_entityId)
+	{
+		auto iter = m_indexMap.find(a_entityId);
+
+		if (iter == m_indexMap.end())
+		{
+			return false;
+		}
+
+		auto index = iter->second;
+
+		auto* ptr = GetComponentFromIndex(index);
+
+		m_factory->Destruct(ptr, false);
+		
+		std::memset(ptr, 0, m_elementSize);
+
+		m_availableIndexQueue.push(index);
+
+		return true;
+	}
+
+	void
+	ComponentPool::Resize(size_type a_newSize)
+	{
+		// If the requested size is larger than the current container
+		if (m_currentSize < a_newSize)
+		{
+			m_entityStorage.resize(m_elementSize * a_newSize);
+
+			size_type newAvailableIndex = m_elementSize * (m_currentSize - 1);
+			
+			// Zero out the new elements
+			std::memset(&m_entityStorage[newAvailableIndex], 0, a_newSize - m_currentSize);
+
+			while (newAvailableIndex < m_elementSize * a_newSize)
+			{
+				m_availableIndexQueue.push(newAvailableIndex);
+				newAvailableIndex += m_elementSize;
+			}
+		} else //If the requested size is smaller than the current container
+		{
+			// TODO: Implement down-sizing
+		}
+		
+		m_currentSize = a_newSize;
+	}
+
+	ComponentPool::size_type
+	ComponentPool::AutoCalculateNewSize() const
+	{
+		return m_currentSize * 2;
+	}
+
+	Component*
+	ComponentPool::GetComponentFromIndex(size_type a_index)
+	{
+		size_type offset = a_index * m_elementSize;
+		Component* reinterpretedPtr = reinterpret_cast<Component*>(&m_entityStorage[offset]);
+		return std::launder(reinterpretedPtr);
+	}
+}
---
+++ NextCore/NextCore/Scripting/Detail/ComponentPool.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+#include <queue>
+
+#include "Scripting/EntityId.h"
+
+namespace Next
+{
+	namespace Reflection {
+		struct GenericFactory;
+	}
+
+	class Component;
+}
+
+namespace Next::Detail
+{
+	class ComponentPool
+	{
+	public:
+		using size_type = uint32_t;
+
+		explicit
+		ComponentPool(Reflection::GenericFactory const* a_factory, size_type a_initialSize = 0);
+
+		Component*
+		AddComponent(EntityId a_entityId);
+
+		Component*
+		GetComponent(EntityId a_entityId);
+
+		Component const*
+		GetComponent(EntityId a_entityId) const
+		{
+			return const_cast<ComponentPool*>(this)->GetComponent(a_entityId);
+		}
+
+		bool
+		RemoveComponent(EntityId a_entityId);
+
+		void
+		Resize(size_type a_newSize);
+
+	private:
+		size_type
+		AutoCalculateNewSize() const;
+	
+		Component*
+		GetComponentFromIndex(size_type a_index);
+	
+		Component const*
+		GetComponentFromIndex(size_type a_index) const
+		{
+			return const_cast<ComponentPool*>(this)->GetComponentFromIndex(a_index);
+		}
+	
+	private:
+		std::vector<std::byte> m_entityStorage;
+
+		std::queue<size_type> m_availableIndexQueue;
+		std::unordered_map<EntityId, size_type> m_indexMap;
+
+		// Use raw pointer here because the lifetime of factories is until program end once constructed
+		Reflection::GenericFactory const* const m_factory;
+		
+		const size_type m_elementSize;
+		
+		size_type m_currentSize;
+
+	};
+}
