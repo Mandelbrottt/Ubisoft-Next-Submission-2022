6eb3bfa3ae69a9814335133bf1fde4145be618b8
Myles Cragg <cragg.myles@gmail.com>
2022-02-28 20:09:36 -0500

    Added cubemap

---
+++ NextCore/NextCore/Graphics/CubeMap.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "CubeMap.h"
+
+#include <filesystem>
+
+namespace Next
+{
+	bool
+	CubeMap::LoadFromFiles(CubeMapFaceDescriptor a_descriptors[6])
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			auto const& descriptor = a_descriptors[i];
+			if (std::filesystem::exists(descriptor.filename))
+			{
+				return false;
+			}
+		}
+		
+		for (int i = 0; i < 6; i++)
+		{
+			auto& [direction, filename] = a_descriptors[i];
+			
+			auto* sprite = new CSimpleSprite(filename.data());
+
+			auto index = static_cast<uint8_t>(direction);
+
+			m_faces[index] = sprite;
+		}
+
+		return true;
+	}
+}
---
+++ NextCore/NextCore/Graphics/CubeMap.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+class CSimpleSprite;
+
+namespace Next
+{
+	namespace Renderer
+	{
+		void
+		Flush();
+	}
+
+	enum class CubeMapFaceDirection : std::uint8_t
+	{
+		Front = 0,
+		Back,
+		Top,
+		Bottom,
+		Left,
+		Right,
+	};
+
+	struct CubeMapFaceDescriptor
+	{
+		CubeMapFaceDirection direction;
+
+		std::string_view filename;
+	};
+
+	class CubeMap final
+	{
+		friend
+		void
+		Renderer::Flush();
+
+	public:
+		bool
+		LoadFromFiles(CubeMapFaceDescriptor a_descriptors[6]);
+
+	private:
+		CSimpleSprite* m_faces[6];
+	};
+}
