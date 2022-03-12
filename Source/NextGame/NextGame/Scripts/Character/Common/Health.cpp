b38ebd110048d245e1cefeaef29b6d8049eb5592
Myles Cragg <cragg.myles@gmail.com>
2022-03-12 14:06:34 -0500

    Added simple health script

---
+++ Source/NextGame/NextGame/Scripts/Character/Common/Health.cpp
@@ -0,0 +1,20 @@
+﻿#include "Health.h"
+
+ReflectRegister(Health);
+
+void
+Health::SubtractHealth()
+{
+	m_health -= 1;
+
+	if (m_health <= 0)
+	{
+		Kill();
+	}
+}
+
+void
+Health::Kill()
+{
+	GetEntity().Destroy();
+}
---
+++ Source/NextGame/NextGame/Scripts/Character/Common/Health.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <MinimalInclude.h>
+
+class Health : public Next::Behaviour
+{
+	ComponentDeclare(Health, Next::Behaviour)
+
+public:
+	float
+	GetHealth() const
+	{
+		return m_health;
+	}
+
+	void
+	SubtractHealth();
+
+	void
+	Kill();
+	
+private:
+	int m_health;
+
+	ReflectMembers(
+		ReflectField(m_health)
+	)
+};
