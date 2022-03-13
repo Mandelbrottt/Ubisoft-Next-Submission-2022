c5ebdb44e6bad43afe78edf5bc0b6c3f4905a27c
Myles Cragg <cragg.myles@gmail.com>
2022-03-13 04:19:10 -0400

    Added commented out other starting scenes for easier debugging

--- Source/NextGame/NextGame/Config/StartingScene.cpp
+++ Source/NextGame/NextGame/Config/StartingScene.cpp
@@ -1,5 +1,8 @@
 #include <Application/Init.h>
 
 #include "Scenes/FlatGroundScene.h"
+#include "Scenes/LevelSelectScene.h"
+#include "Scenes/MenuScene.h"
 
 StartingScene(FlatGroundScene);
+//StartingScene(MenuScene);
